b864dce2154e2989fd4b555a60aad4f5aadf7be0
yuefufufu <104189406+yuefufufu@users.noreply.github.com>
2022-08-09 18:25:43 +0900

    Update noetic_setup.sh

--- noetic_setup.sh
+++ noetic_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd 
+cd
+sudo apt update
+sudo apt upgrade
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
@@ -9,7 +11,6 @@ sudo apt install -y ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source /opt/ros/noetic/setup.bash
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install -y python3-rosdep
 sudo apt install -y python3-osrf-pycommon python3-catkin-tools
 sudo apt upgrade -y
 sudo rosdep init
